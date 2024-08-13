ec417f754e5e7da0049e3ec3431bb88073282145
j-hiyama <j-hiyama@iret.co.jp>
2024-08-13 20:24:36 +0900

    add

---
+++ CdkWorkshopStack/lib/cdk_workshop_stack-stack.ts
@@ -0,0 +1,14 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+
+export class CdkWorkshopStackStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const bucket = new s3.Bucket(this, 'CreateBucket', {
+      bucketName: "tetete-bucket-20221231",
+      versioned: true
+    });
+  }
+}
